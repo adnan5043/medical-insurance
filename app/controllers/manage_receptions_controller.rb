5e655cd1fbcfc30bf5bda9d267d4272960047ebf
Adnan Naeem <adnannaeem785@gmail.com>
2025-01-21 13:32:52 +0500

    Updated reception tabel associated with user tabel

--- app/controllers/manage_receptions_controller.rb
+++ app/controllers/manage_receptions_controller.rb
@@ -1,8 +1,9 @@
 class ManageReceptionsController < ApplicationController
   before_action :set_manage_reception, only: %i[show edit update destroy]
+
   def index
-    @manage_receptions = ManageReception.page(params[:page]) 
-    @manage_reception = ManageReception.new
+    @manage_receptions = ManageReception.includes(:userable).page(params[:page])
+    @user = User.new 
   end
 
   def show
@@ -10,31 +11,55 @@ class ManageReceptionsController < ApplicationController
 
   def new
     @manage_reception = ManageReception.new
+    @user = User.new 
   end
 
   def create
-    @manage_reception = ManageReception.new(manage_reception_params)
-    if @manage_reception.save
-      redirect_to manage_receptions_path, notice: 'Reception was successfully created.'
-    else
-      render :new, alert: 'There were errors creating the Reception Manager.'
+    ActiveRecord::Base.transaction do
+      @user = User.new(user_params) 
+      @manage_reception = ManageReception.new
+
+      if @user.save
+        @manage_reception.userable = @user # Associate the user with ManageReception
+        if @manage_reception.save
+          redirect_to manage_receptions_path, notice: 'Reception was successfully created.'
+        else
+       redirect_to manage_receptions_path, alert: 'There were errors creating the Reception Manager.'
+        end
+      else
+      redirect_to manage_receptions_path, alert: "There were errors creating the User: #{@user.errors.full_messages.join(', ')}"
+      end
     end
+  rescue ActiveRecord::RecordInvalid
+    redirect_to manage_receptions_path, alert: 'Transaction failed. Please try again.'
   end
 
   def edit
+    @user = @manage_reception.userable 
   end
 
   def update
-    if @manage_reception.update(manage_reception_params)
-      redirect_to manage_receptions_path, notice: 'Reception  was successfully updated.'
-    else
-      redirect_to manage_receptions_path, alert: "errors: #{@branch.errors.full_messages.join(', ')}"
+    ActiveRecord::Base.transaction do
+      @user = @manage_reception.userable
+      if @user.update(user_params) && @manage_reception.update(manage_reception_params)
+        redirect_to manage_receptions_path, notice: 'Reception was successfully updated.'
+      else
+        redirect_to manage_receptions_path, alert: "Errors: #{@user.errors.full_messages.join(', ')}"
+      end
     end
+  rescue ActiveRecord::RecordInvalid
+    redirect_to manage_receptions_path, alert: 'Transaction failed. Please try again.'
   end
 
   def destroy
-    @manage_reception.destroy
-    redirect_to manage_receptions_url, notice: 'Reception was successfully deleted.'
+    ActiveRecord::Base.transaction do
+      @user = @manage_reception.userable
+      @manage_reception.destroy
+      @user.destroy
+      redirect_to manage_receptions_url, notice: 'Reception was successfully deleted.'
+    end
+  rescue ActiveRecord::RecordInvalid
+    redirect_to manage_receptions_url, alert: 'There were errors deleting the Reception Manager.'
   end
 
   private
@@ -45,14 +70,24 @@ class ManageReceptionsController < ApplicationController
 
   def manage_reception_params
     params.require(:manage_reception).permit(
-      :first_name, 
-      :last_name, 
-      :username, 
-      :phone, 
-      :address, 
-      :avatar, 
-      :employee_designation, 
-      :joining_date
     )
+  end
+
+  def user_params
+    if params[:manage_reception] && params[:manage_reception][:user]
+      # Handle nested user params
+      params.require(:manage_reception).require(:user).permit(
+        :first_name, :last_name, :phone, :address, :avatar, 
+        :employee_designation, :joining_date, :email, 
+        :password, :password_confirmation
+      )
+    else
+      # Handle direct user params for edit (when not nested)
+      params.require(:user).permit(
+        :first_name, :last_name, :phone, :address, :avatar, 
+        :employee_designation, :joining_date, :email, 
+        :password, :password_confirmation
+      )
+    end
   end
 end
--- app/models/manage_reception.rb
+++ app/models/manage_reception.rb
@@ -1,6 +1,3 @@
 class ManageReception < ApplicationRecord
-  validates :first_name, :last_name, :username, :phone, :employee_designation, :joining_date, presence: true
-  # validates :username, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email" }, uniqueness: true
-  # validates :phone, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit number" }
-  has_one_attached :avatar
+  belongs_to :userable, polymorphic: true
 end
